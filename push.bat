c67de3545266ab0acb99728d46a85988240db2b1
肖玉明 <xiaoymin@foxmail.com>
2018-05-02 15:25:46 +0800

    push.bat

---
+++ push.bat
@@ -0,0 +1,14 @@
+cd D:\Users\xiaoymin\code\github\hugo-blog\public
+d:
+xcopy * D:\Users\xiaoymin\code\github\xiaoymin.github.io /S/Y
+
+cd D:\Users\xiaoymin\code\github\xiaoymin.github.io
+
+D:
+
+git add *
+
+git commit -m new-blogs
+
+git push -u origin master
+
